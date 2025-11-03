ace577d35134983cdca63436e9a36bf5d66355d6
Max <rcmaksim@gmail.com>
2025-11-04 00:48:36 +0300

    Version 1.0

--- app/src/main/java/com/example/rectanglesapp/MainActivity.kt
+++ app/src/main/java/com/example/rectanglesapp/MainActivity.kt
@@ -4,44 +4,134 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.foundation.layout.statusBars
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.windowInsetsPadding
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.integerResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.rectanglesapp.ui.theme.RectanglesAppTheme
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            RectanglesAppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+            MyScreen()
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun MyScreen() {
+    val elements = rememberSaveable { mutableStateListOf<Int>() }
+
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(colorResource(id = R.color.background))
+            .windowInsetsPadding(WindowInsets.statusBars)
+    ) {
+        LazyVerticalGrid(
+            columns = GridCells.Fixed(count = integerResource(id = R.integer.columns_count)),
+            verticalArrangement = Arrangement.spacedBy(10.dp),
+            horizontalArrangement = Arrangement.spacedBy(10.dp),
+            modifier = Modifier
+                .fillMaxWidth()
+                .align(Alignment.TopCenter)
+                .padding(
+                    start = 10.dp, end = 10.dp,
+                    bottom = 100.dp
+                )
+        ) {
+            items(
+                items = elements,
+                key = { it }
+            ) { value ->
+                Square(value)
+            }
+        }
+        Row(
+            modifier = Modifier
+                .align(Alignment.BottomCenter)
+                .height(110.dp)
+                .padding(
+                    top = 30.dp,
+                    bottom = 30.dp
+                ),
+        ){
+            BottomButton({ elements.add(elements.size) }, stringResource(R.string.Add_button))
+            BottomButton({elements.removeLastOrNull()}, stringResource(R.string.Remove_button))
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    RectanglesAppTheme {
-        Greeting("Android")
+private fun MyScreenPreview(){
+    MyScreen()
+}
+
+@Composable
+private fun BottomButton(action : () -> Unit, text : String){
+    Button(
+        onClick = action,
+        modifier = Modifier
+            .width(190.dp)
+            .padding(
+                start = 5.dp,
+                end = 5.dp
+            ),
+
+        colors = ButtonDefaults.buttonColors(
+            containerColor = colorResource(id = R.color.button)),
+        shape = RoundedCornerShape(16.dp)
+    ) {
+        Text(
+            text = text,
+            color = colorResource(R.color.main_text),
+            fontSize = 34.sp,
+        )
+    }
+}
+
+@Composable
+private fun Square(number: Int) {
+    val col = if (number % 2 == 0) colorResource(R.color.red) else colorResource(R.color.blue)
+    Box(
+        modifier = Modifier
+            .aspectRatio(1f)
+            .background(col)
+    ) {
+        Text(
+            text = "$number",
+            color = colorResource(R.color.main_text),
+            fontSize = 34.sp,
+            modifier = Modifier.align(Alignment.Center)
+        )
     }
 }
